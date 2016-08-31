c1538cc31bbbc8f0cec18d9fdd1d2ebb0a02ba37
Phil Boothe <pfb3cn@virginia.edu>
2016-08-31 14:37:55 -0400

    added filter functionality (sans AJAX)

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.db.models import Q
 from .models import User
 from .forms import UserForm
-from django.views.generic import View
 
 class Users(View):
     def get(self, request):
-        userquery = User.objects.all()
+        userquery = User.objects.all()[0:5]
         context = {
             'users': userquery,
             'add_user': UserForm(),
@@ -22,4 +23,18 @@ class Users(View):
         return redirect('index')
 
 class Filter(View):
-    def post(self, request)
+    # def get(self, request):
+    #     userquery = User.objects.all()
+    #     context = { 'users': userquery }
+    #     return render(request, 'users/index_table.html', context)
+
+    def post(self, request):
+        userquery = User.objects.all()
+        if request.POST['name']:
+            userquery = userquery.filter(Q(first_name__contains=request.POST['name']) | Q(last_name__contains=request.POST['name']))
+        if request.POST['date_from']:
+            userquery = userquery.filter(created_at__gte=request.POST['date_from'])
+        if request.POST['date_to']:
+            userquery = userquery.filter(created_at__lte=request.POST['date_to'])
+        context = { 'users': userquery[0:5], 'add_user': UserForm() }
+        return render(request, 'users/index.html', context)
